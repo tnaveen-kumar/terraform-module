caa5d67181f8fcf494ee21bc2de6dc68b1fc1509
tnaveen-kumar <123799811+tnaveen-kumar@users.noreply.github.com>
2025-09-02 16:09:36 +0530

    Add files via upload

---
+++ resource.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg-1" {
+    name     = var.rg-name
+    location = var.rg-location
+}
---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "rg-name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "rg-location" {
+  description = "The location of the resource group"
+  type        = string
+
+}
